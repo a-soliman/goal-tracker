ecb70afe6fd2f3a75ddf1f52f22a611871522ad3
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-18 09:27:05 -0700

    started the database and configured the server

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ const path          = require('path');
 
 const Goal          = require('./models/goals')
 
+mongoose.connect('mongodb://ahmed_soliman:123456@ds247699.mlab.com:47699/goal-tracker');
+const db            = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('DB connected successfully!');
+});
+const app           = express()
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', ( req, res ) => {
+    res.send('It works');
+});
 
 app.listen(3333, () => {
     console.log('app is runing on port 3333.');
