e8f657696d604e73a50c04b1d01bf28fbfd37ef7
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-19 07:37:52 -0700

    coded a addGoal method and attached the new goal to the UI

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,33 @@
 function getGoals() {
     $.get('http://localhost:3333/goals', ( data ) => {
         viewModel.goals(data);
-        console.log(data)
     });
 }
 
 function ViewModel() {
     const self = this;
 
+    // STORES THE GOALS ARRY AFTER THE AJAX CALL
     self.goals = ko.observableArray();
+    
+    // FORM VALUES
+    self.goalInputName      = ko.observable();
+    self.goalInputType      = ko.observable();
+    self.goalInputDeadline  = ko.observable();
+
+    self.addGoal = function () {
+        let name        = self.goalInputName();
+        let type        = self.goalInputType();
+        let deadline    = self.goalInputDeadline();
+
+        const newGoal = { name, type, deadline };
+
+        self.goals.push(newGoal);
+        
+
+    };
+
+    self.goalTypes = ko.observableArray(['Health & Fitness', 'Profeessional', 'Family & Relationships', 'Self help'])
 }
 
 let viewModel = new ViewModel();
